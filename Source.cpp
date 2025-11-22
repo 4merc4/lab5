0db245b17db5fe71577962193308c88935a53920
4merc4 <96210094+4merc4@users.noreply.github.com>
2025-11-22 20:16:05 +0200

    Refactor Source.cpp to clean up formatting

--- Source.cpp
+++ Source.cpp
@@ -7,51 +7,47 @@
 
 using namespace std;
 
-// ===========================================================
-// Симуляція обчислень
-// ===========================================================
+
 
 void compute(const string& name, int seconds) {
     this_thread::sleep_for(chrono::seconds(seconds));
     osyncstream(cout) << name << "\n";
 }
 
-// Швидке
+
 void quick(const string& name) {
     compute(name, 1);
 }
 
-// Повільне
+
 void slow(const string& name) {
     compute(name, 7);
 }
 
-// ===========================================================
-// Робоча схема
-// ===========================================================
+
 
 void work() {
     auto start = chrono::steady_clock::now();
 
-    // Потік гілки A
+    
     auto futureA = async(std::launch::async, [] {
         quick("A1");
         slow("A2");
         quick("A3");
         });
 
-    // Потік гілки B
+    
     auto futureB = async(std::launch::async, [] {
         quick("B1");
         slow("B2");
         quick("B3");
         });
 
-    // Чекаємо завершення обох гілок
+    
     futureA.get();
     futureB.get();
 
-    // Тепер виконуємо C (залежить від A3 і B3)
+    
     quick("C");
 
     auto end = chrono::steady_clock::now();
@@ -62,11 +58,9 @@ void work() {
         << seconds << " seconds.\n";
 }
 
-// ===========================================================
-// Демонстрація
-// ===========================================================
 
 int main() {
     work();
     return 0;
 }
+
